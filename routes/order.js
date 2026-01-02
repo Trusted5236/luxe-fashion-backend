6c59a2864b5cbf4f6f852f768693942b6b5bf4c3
Agbata Trust <trustagbata@gmail.com>
2026-01-02 10:25:26 +0100

    new-endpoints

--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,15 @@
-import express from "express";
+import express, { application } from "express";
 import authMiddleWare from "../middleware/auth.js";
 import Cart from "../models/cartSchema.js";
 import Order from "../models/orderModal.js";
+import axios from "axios";
+import { paypal, getAccessToken } from "../config/paypal.js";
+import dotenv from 'dotenv'
 const router = express.Router()
+dotenv.config()
 
 
-router.post("/", authMiddleWare, async (req, res)=>{
+router.post("/create", authMiddleWare, async (req, res)=>{
     const {firstName, lastName, email, phone, address, city, state, country, zip} = req.body
     const userId = req.user.id
 
@@ -42,4 +46,113 @@ router.post("/", authMiddleWare, async (req, res)=>{
     res.status(201).json({message: "Order created, awaiting payment", orderId: newOrder._id, order : newOrder});
 })
 
+router.post("/paypal/create-order", authMiddleWare, async (req, res)=>{
+    const userId = req.user.id
+    const {orderId} = req.body
+    const order = await Order.findById(orderId)
+
+     if (!orderId) {
+            return res.status(400).json({ message: "Order ID is required" });
+        }
+
+    if (!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+
+    if (order.user.toString() !== userId.toString()) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+
+
+    const response = await axios.post(`${paypal.baseUrl}/v2/checkout/orders`, {
+        intent : "CAPTURE",
+        purchase_units: [{
+            description : "Order Description",
+            amount : {
+                currency_code : "USD",
+                value : order.totalPrice.toFixed(2)
+            }
+        }],
+        application_context : {
+            return_url : `${process.env.Frontend_URL}/checkout/success`,
+            cancel_url : `${process.env.Frontend_URL}/checkout/cancel`,
+            brand_name : "LUXE Fashion",
+            user_action : "PAY_NOW"
+        }
+        
+    }, {
+        headers : {
+            "Content-Type" : "application/json",
+            Authorization : `Bearer ${await getAccessToken()}`
+        }
+    }
+)
+
+const paypalOrder =  response.data
+
+res.json({
+    success : true,
+    paypalOrderId : paypalOrder.id,
+    orderId : order._id,
+    approvalUrl : response.links.find(link => link.rel === "approve").href,
+})
+})
+
+
+router.post("/paypal/capture-order", authMiddleWare, async (req, res)=>{
+    const {paypalOrderId, orderId} = req.body
+    const userId = req.user.id
+
+    if (!paypalOrderId || !orderId) {
+        return res.status(400).json({ 
+            message: "PayPal Order ID and Order ID are required" 
+        });
+    }
+
+    const order =  await Order.findById(orderId)
+
+    if (!order) {
+            return res.status(404).json({ message: "Order not found" });
+    }
+
+    if (order.user.toString() !== userId.toString()) {
+            return res.status(403).json({ message: "Unauthorized" });
+    }
+
+    const response = await axios.post(`${paypal.baseUrl}/v2/checkout/orders/${paypalOrderId}/capture`, {}, {
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization" : `Bearer ${await getAccessToken()}`
+        }
+    })
+
+    const captureData = response.data;
+
+    if (captureData.status === "COMPLETED") {
+            
+            order.orderStatus = "Paid";
+            order.paymentId = captureData.id;
+            await order.save();
+
+            
+            await Cart.findOneAndDelete({ user: userId });
+
+            res.json({
+                success: true,
+                message: "Payment successful",
+                order: {
+                    orderId: order._id,
+                    orderStatus: order.orderStatus,
+                    paymentId: order.paymentId,
+                    totalPrice: order.totalPrice
+                }
+            });
+        } else {
+            res.status(400).json({
+                success: false,
+                message: "Payment failed",
+                status: captureData.status
+            });
+        }
+})
 export default router
