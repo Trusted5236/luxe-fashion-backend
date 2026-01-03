88d2e912d7b922ced039b4893466f76b8bd03dc2
Agbata Trust <trustagbata@gmail.com>
2026-01-03 19:56:36 +0100

    fix

--- routes/admin.js
+++ routes/admin.js
@@ -9,14 +9,22 @@ import Order from "../models/orderModal.js"
 router.use(authMiddleWare, checkRole("admin"))
 
 router.get('/stats', async (req, res)=>{
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
     const totalUsers = await User.countDocuments({role : "user"})
     const totalSellers = await User.countDocuments({role : "seller"})
     const totalAdmins = await User.countDocuments({role : "admin"})
-
     const totalProducts = await Product.countDocuments()
+    const users = await User.find({role : "user"}).select("-password -__v").sort({createdAt : -1}).skip(skip).limit(limit)
+    const sellers = await User.find({role : "seller"}).select("-password -__v").sort({createdAt : -1}).skip(skip).limit(limit)
+    const admins = await User.find({role : "seller"}).select("-password -__v").sort({createdAt : -1}).skip(skip).limit(limit)
+    const products = await Product.find();
+
 
     const revenue = await Order.aggregate([
-        {$match : {orderStatus : "completed"}},
+        {$match : {orderStatus : "Paid"}},
         {$group : {_id: null, total : {$sum : `$totalPrice`}}}
     ])
 
@@ -37,15 +45,28 @@ router.get('/stats', async (req, res)=>{
     res.json({
             users: {
                 total: totalUsers,
-                newThisMonth: newUsersThisMonth
+                newThisMonth: newUsersThisMonth,
+                list: users
             },
             sellers: {
                 total: totalSellers,
-                newThisMonth: newSellersThisMonth
+                newThisMonth: newSellersThisMonth,
+                list: sellers
             },
-            admins: totalAdmins,
-            products: totalProducts,
-            revenue: revenue[0]?.total || 0
+            admins: {
+                total: totalAdmins,
+                list: admins
+            },
+            products: {
+                total: totalProducts,
+                list: products
+            },
+            revenue: revenue[0]?.total || 0,
+            pagination: {
+                page,
+                limit,
+                totalPages: Math.ceil(totalUsers / limit)
+            }
         });
 })
 
